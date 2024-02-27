7b0c08541cf2311c00a3b3e47002bf02395b157d
Steven Gong <gonghaima@hotmail.com>
2024-02-28 00:08:00 +1100

    overall structure of home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,36 @@
+import {Col, Row} from 'antd';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Row
+      gutter={[32, 32]}
+      style={{
+        marginTop: '32px',
+      }}
+      >
+        <Col 
+        xs={24}
+        sm={24}
+        xl={8}
+        style={{
+          height: '460PX',
+        }}  
+        >
+          CalendarUpcomingEvents
+        </Col>
+        <Col 
+        xs={24}
+        sm={24}
+        xl={8}
+        style={{
+          height: '460PX',
+        }}  
+        >
+          DashboardDealsChart
+        </Col>
+      </Row>
+    </div>
   )
 }
 
