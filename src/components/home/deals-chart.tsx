797f0c2cb20f4777f20ae8b73ec061a98dbac221
Steven Gong <gonghaima@hotmail.com>
2024-03-22 23:08:54 +1100

    top section of chart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,10 +1,32 @@
+import { DollarOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
 import React from 'react'
+import { Text } from '../text';
 
 type Props = {}
 
 const DealsChart = (props: Props) => {
   return (
-    <div>DealsChart</div>
+    <Card 
+    style={{height: '100%'}}
+    headStyle={{padding: '8px 16px'}}
+    bodyStyle={{padding: '24px 24px 0 24px'}}
+    title={
+      <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: '8px',
+      
+      }}
+      >
+        <DollarOutlined/>
+        <Text size="sm" style={{marginLeft: '.5rem'}}>
+          Deals chart
+        </Text>
+      </div>
+    }
+    ></Card>
   )
 }
 
